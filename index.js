92fc2b3f61c118ca43920647bbe05738c2c01b62
kinantisaaz@gmail.com <kinantisaaz@gmail.com>
2022-06-04 01:20:21 +0700

    first commit

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const swagger = require("swagger-ui-express");
 dotenv.config();
+
+// test
+app.get("/", (req, res) => {
+  res.send("Hello from Tanya!");
+});
+
+// swagger
+const apiDocs = require("./tanyaDocs.json");
+app.use("/tanyaDocs", swagger.serve, swagger.setup(apiDocs));
 
 //connect DB
 mongoose.connect(process.env.access_db, () => console.log("check database"));
@@ -12,7 +21,7 @@ mongoose.connect(process.env.access_db, () => console.log("check database"));
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const tiketRoute = require("./routes/TiketBus");
-const transaksiRoute = require("./routes/Transaksi");
+const transaksiRoute = require("./routes/transaksi");
 // const paymentRoute = require("./routes/payment");
 
 //middleware | run when hits routes
@@ -25,5 +34,8 @@ app.use("/tanya/tiket", tiketRoute);
 app.use("/tanya/transaksi", transaksiRoute);
 // app.use("/tanya/payment", paymentRoute);
 
-//running app
-app.listen(process.env.port || 3000, () => console.log("server ok!"));
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
